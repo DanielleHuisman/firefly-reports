a41d1c585c04d4304c2c114693a36c364eea03b7
Daniëlle Huisman <danielle@huisman.me>
2026-06-14 19:19:57 +0200

    feat: add profit and loss report

--- packages/api/src/report.rs
+++ packages/api/src/report.rs
@@ -1,5 +1,6 @@
 use dioxus::prelude::*;
 
+use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
 #[cfg(feature = "server")]
 use shield_dioxus_axum::{ExtractShield, UserRequired};
@@ -7,7 +8,19 @@ use shield_dioxus_axum::{ExtractShield, UserRequired};
 use shield_memory::User as ShieldUser;
 
 #[derive(Deserialize, PartialEq, Serialize)]
-pub struct ProfitAndLossReport {}
+pub struct ProfitAndLossReport {
+    pub credit: Vec<ReportCategory>,
+    pub credit_total: Decimal,
+    pub debit: Vec<ReportCategory>,
+    pub debit_total: Decimal,
+}
+
+#[derive(Deserialize, PartialEq, Serialize)]
+pub struct ReportCategory {
+    pub id: String,
+    pub name: String,
+    pub amount: Decimal,
+}
 
 #[get("/api/profit-and-loss-report?started_at&ended_at", shield: ExtractShield<ShieldUser>, user: UserRequired<ShieldUser>)]
 pub async fn profit_and_loss_report(
@@ -20,6 +33,8 @@ pub async fn profit_and_loss_report(
         categories_api::ListCategoryParams,
         insight_api::{InsightExpenseCategoryParams, InsightIncomeCategoryParams},
     };
+    use itertools::Itertools;
+    use rust_decimal::dec;
 
     use crate::client::firefly_client;
 
@@ -30,8 +45,6 @@ pub async fn profit_and_loss_report(
         .list_category(ListCategoryParams::builder().limit(100).page(1).build())
         .await
         .context("failed to list categories")?;
-
-    info!("{categories:#?}");
 
     let income_entries = client
         .insight_api()
@@ -55,8 +68,72 @@ pub async fn profit_and_loss_report(
         .await
         .context("failed to list expenses by category")?;
 
-    info!("{income_entries:#?}");
-    info!("{expense_entries:#?}");
+    let entries = income_entries
+        .into_iter()
+        .chain(expense_entries)
+        .sorted_by(|a, b| a.id.cmp(&b.id))
+        .chunk_by(|entry| entry.id.clone());
 
-    Ok(ProfitAndLossReport {})
+    let categories: Vec<ReportCategory> = entries
+        .into_iter()
+        .filter_map(|(category_id, entries)| {
+            category_id.and_then(|category_id| {
+                categories
+                    .data
+                    .iter()
+                    .find(|category| category.id == category_id)
+                    .map(|category| {
+                        entries
+                            .into_iter()
+                            .try_fold(dec!(0), |acc, entry| {
+                                // TODO: Support multiple currencies.
+
+                                entry
+                                    .difference
+                                    .map(|difference| Decimal::from_str_exact(&difference))
+                                    .unwrap_or(Ok(dec!(0)))
+                                    .map(|difference| acc + difference)
+                            })
+                            .map(|amount| ReportCategory {
+                                id: category.id.clone(),
+                                name: category.attributes.name.clone(),
+                                amount,
+                            })
+                    })
+            })
+        })
+        .try_collect()
+        .context("failed to process entries")?;
+
+    let (mut credit, mut debit): (Vec<_>, Vec<_>) = categories
+        .into_iter()
+        .partition(|category| category.amount >= dec!(0));
+
+    for entry in &mut credit {
+        entry.amount = entry.amount.round_dp(2);
+    }
+    for entry in &mut debit {
+        entry.amount = (-entry.amount).round_dp(2);
+    }
+
+    credit.sort_by(|a, b| a.name.cmp(&b.name));
+    debit.sort_by(|a, b| a.name.cmp(&b.name));
+
+    let credit_total = credit
+        .iter()
+        .map(|category| category.amount)
+        .sum::<Decimal>()
+        .round_dp(2);
+    let debit_total = debit
+        .iter()
+        .map(|category| category.amount)
+        .sum::<Decimal>()
+        .round_dp(2);
+
+    Ok(ProfitAndLossReport {
+        credit,
+        credit_total,
+        debit,
+        debit_total,
+    })
 }
